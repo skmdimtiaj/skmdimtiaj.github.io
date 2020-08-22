e967baaed1721f2a1c7e96e44fa3f02e05d9e76c
sk md imtiaj <46376967+skmdimtiaj@users.noreply.github.com>
2020-08-22 21:20:54 +0530

    Update all_blogger_alert.js

--- all_blogger_alert.js
+++ all_blogger_alert.js
@@ -4,5 +4,5 @@ function get_url_param(query){
 		   return urlParams.get(query);
 		}
 if(get_url_param('tos') != 'true'){
-  document.write(`<iframe id="ref" src="https://github.io.boysofts.com/my_iframe.html" style="position:absolute;top:0;left:0;width:100%;height:100%;background-color: rgba(0,0,0,0.8);z-index:2;"></iframe>`);
+  document.write(`<iframe id="ref" src="https://github.io.boysofts.com/my_iframe.html" style="position:absolute;top:0;left:0;width:100%;height:100%;background-color: rgba(0,0,0,0.9);z-index:2;"></iframe>`);
 }
